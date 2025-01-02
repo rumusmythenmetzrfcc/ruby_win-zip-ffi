11ef34f2f15d47b152b7cc6875f299bc6a6f4746
Rumus Mythenmetz <rumus.mythenmetz.afrcc@gmail.com>
2025-01-02 14:58:15 +0100

    bundle gem init

---
+++ lib/ruby_win/zip/ffi.rb
@@ -0,0 +1,12 @@
+# frozen_string_literal: true
+
+require_relative "ffi/version"
+
+module RubyWin
+  module Zip
+    module Ffi
+      class Error < StandardError; end
+      # Your code goes here...
+    end
+  end
+end
---
+++ spec/ruby_win/zip/ffi_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe RubyWin::Zip::Ffi do
+  it "has a version number" do
+    expect(RubyWin::Zip::Ffi::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
